2c9a56dbb0731973cf68fc6d8373cebb9f9f9c61
tian <tianwei0629@126.com>
2017-11-04 15:20:18 +0800

    zzdemo1

--- src/com/ctrip/desc/service.java
+++ src/com/ctrip/desc/service.java
@@ -3,7 +3,9 @@ package com.ctrip.desc;
 public class service {
 
 	public static void main(String[] args) {
-          System.out.println("hello everybody");       
+          System.out.println("hello everybody");   
+          
+          System.out.println("hello me");   
          
 	}
 }
