a6531546cadba13aa1c4532c490adf7241643834
tian <tianwei0629@126.com>
2017-11-04 16:12:57 +0800

    增添1,2

---
+++ src/com/ctrip/desc/service2.java
@@ -0,0 +1,12 @@
+package com.ctrip.desc;
+
+public class service {
+
+	public static void main(String[] args) {
+          
+		  System.out.println("hello everybody");   
+		  System.out.println("hello everybody 1");   
+		  System.out.println("hello everybody 2");   
+         
+	}
+}
