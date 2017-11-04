62087d159ac165201cc46fad8cbae7e69038d538
tian <tianwei0629@126.com>
2017-11-04 18:49:20 +0800

    develop add

--- src/com/ctrip/desc/service2.java
+++ src/com/ctrip/desc/service2.java
@@ -4,7 +4,11 @@ public class service {
 
 	public static void main(String[] args) {
      
-		 System.out.println("master 2:add");   
+		 System.out.println("master 2:add"); 
+		 	 
+		 System.out.println("===========");  
+		 
+		 System.out.println("develop 2:add");   
          
 	}
 }
