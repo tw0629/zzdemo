1b25da8a5f3fb2e418f5e4767f82fa6b268271f5
tian <tianwei0629@126.com>
2017-11-04 16:14:14 +0800

    增添3,4

--- src/com/ctrip/desc/service2.java
+++ src/com/ctrip/desc/service2.java
@@ -5,7 +5,10 @@ public class service {
 	public static void main(String[] args) {
           
 		  System.out.println("hello everybody");   
-		  System.out.println("hello everybody 1");   
+		  System.out.println("hello everybody 1"); 
+		  
+		  System.out.println("hello everybody 3");   
+		  System.out.println("hello everybody 4");   
 
          
 	}
