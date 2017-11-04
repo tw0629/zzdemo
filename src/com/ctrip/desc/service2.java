f7c33589ca8e565c09112504ebb53879f9c7ca82
tian <tianwei0629@126.com>
2017-11-04 18:39:21 +0800

    删除

--- src/com/ctrip/desc/service2.java
+++ src/com/ctrip/desc/service2.java
@@ -4,18 +4,16 @@ public class service {
 
 	public static void main(String[] args) {
           
-		  System.out.println("hello everybody");   
-		  System.out.println("hello everybody 1"); 
-		  
-		  System.out.println("hello everybody 3"); 
-		  
-		  System.out.println("hello everybody 5");   
-		  
-		  System.out.println("hello everybody 7");   
-		  System.out.println("hello everybody 8");   
-		  
-		  System.out.println("hello everybody 9");   
-		  System.out.println("hello everybody 10");   
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																							  System.out.println("hello everybody");   
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																							  System.out.println("hello everybody 1"); 
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																							  
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																							  System.out.println("hello everybody 3"); 
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																							  
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																							  System.out.println("hello everybody 5");   
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																							  
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																							  System.out.println("hello everybody 7");   
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																							  System.out.println("hello everybody 8");   
+
 
 
          
